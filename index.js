d2c4afc4e532a12e64a59db5daaf9166d5ac1719
avyukd <avyukd@gmail.com>
2018-04-01 13:55:52 -0400

    working css test. js still does not work

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 8000; // you can use any port
-app.listen(port);
-console.log('server on' + port);
+
+app.use(express.static(__dirname+'/public'));
+
 app.get('/', function (req, res) {
-  res.render('index', {});
-});
+})
+
+var server = app.listen(8000)
